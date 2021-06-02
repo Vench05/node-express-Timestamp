8e7dce951572e58f2ea6e9b9bffd181785fc9988
Vench05 <ralfvenchbillones@gmail.com>
2021-06-02 20:49:59 +0800

    update

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const app = express()
 const port = 3000
 
 app.get('/api/:date', (req, res) => {
-    const date = req.params.date.split("-")
-    const utc = Date.parse(date)
-    if (!utc) res.send({"error": "Invalid Date"})
-    res.send({"unix": utc | date, "utc": new Date(date)})
+    let date = new Date(req.params.date)
+    if(date == 'Invalid Date') {date =  new Date(parseInt(req.params.date))}
+    if(date == 'Invalid Date') res.send({"error": "Invalid Date"})
+    res.send({"unix": date.getTime(), "utc": date.toString()})
 })
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
