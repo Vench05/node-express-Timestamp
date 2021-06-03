97cf97f9eca2e57b766f0f5754b3237e4c2707dd
Vench05 <ralfvenchbillones@gmail.com>
2021-06-03 10:02:00 +0800

    front-end

--- app.js
+++ app.js
@@ -4,20 +4,29 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// app.use(express.json())
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
 
 app.get('/api', (req, res) => {
     let date = new Date()
     date = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()))
-    res.send({"unix": date.getTime(), "utc": date.toUTCString()})
+    res.json({unix: date.getTime(), utc: date.toUTCString()})
 })
 
-app.get('/api/:date', (req, res) => {
+app.get('/api/:date?', (req, res) => {
     let date = new Date(req.params.date)
-    console.log(date);
-    if(date == 'Invalid Date') {date =  new Date(parseInt(req.params.date))}
-    if(date == 'Invalid Date') res.send({"error": "Invalid Date"})
+    if (!isNaN(req.params.date)){
+        date = new Date(parseInt(req.params.date))
+    }
+    if(date.toString() === 'Invalid Date') res.json({error: date.toString()})
     date = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()))
-    res.send({ "unix": date.getTime(), "utc": date.toUTCString() })
+    res.json({ unix: date.getTime(), utc: date.toUTCString() })
 })
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
