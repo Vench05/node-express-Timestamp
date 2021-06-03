478f81dcb813077ac3d665b72ccf0366c4eab915
Vench05 <ralfvenchbillones@gmail.com>
2021-06-03 08:51:31 +0800

    UTCString

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get('/api/:date', (req, res) => {
     let date = new Date(req.params.date)
     if(date == 'Invalid Date') {date =  new Date(parseInt(req.params.date))}
     if(date == 'Invalid Date') res.send({"error": "Invalid Date"})
-    res.send({"unix": date.getTime(), "utc": date.toString()})
+    res.send({"unix": date.getTime(), "utc": date.toUTCString()})
 })
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
